d89230831e57f9128087177e1d47d9d4bee975e0
Brendan Murty <brendan@bcm.works>
2026-09-16 13:12:25 +1000

    Remove outdated Git Hooks package, rebuild CLI binary

--- cli/task_setup.go
+++ cli/task_setup.go
@@ -10,9 +10,6 @@ func TaskSetup() {
 	LogInfo("Installing Deno dependencies")
 	Cmd("deno task install")
 
-	LogInfo("Setup Git Hooks")
-	Cmd("deno task hooks:install")
-
 	LogInfo("Installing Go dependencies")
 	Cmd("cd cli && go mod download")
 
